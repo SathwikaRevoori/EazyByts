119480d12d6949b4898ccbb16273f1045b91d9ba
SathwikaRevoori <23071a12b7@vnrvjiet.in>
2025-06-12 18:28:14 +0530

    fixed commit statements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// ... other imports
-import NotFound from './pages/NotFound'; // Create this component
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import { ThemeProvider } from './context/ThemeContext';
+import AdminDashboard from './pages/AdminDashboard';
+import EditHome from './pages/EditHome';
+import EditProjects from './pages/EditProjects';
+import EditContact from './pages/EditContact';
+import ProtectedRoute from './components/ProtectedRoute';
+import './styles/Admin.css';
 
 function App() {
   return (
@@ -19,12 +29,11 @@ function App() {
           <Route path="/admin/home" element={<ProtectedRoute><EditHome /></ProtectedRoute>} />
           <Route path="/admin/projects" element={<ProtectedRoute><EditProjects /></ProtectedRoute>} />
           <Route path="/admin/contact" element={<ProtectedRoute><EditContact /></ProtectedRoute>} />
-
-          {/* 404 route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </ThemeProvider>
   );
 }
+
 export default App;
