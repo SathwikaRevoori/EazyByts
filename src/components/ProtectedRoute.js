5ed4697d8c00e90282dd82e51d54c3301a4b1cd8
SathwikaRevoori <23071a12b7@vnrvjiet.in>
2025-06-12 16:35:03 +0530

    Updated login flow and ProtectedRoute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import axios from 'axios';
 
-export default function ProtectedRoute({ children }) {
-  const role = localStorage.getItem('userRole') || '';
-  const isAdmin = role === 'admin';
-  return isAdmin ? children : <Navigate to="/login"  replace />; 
-}
+const ProtectedRoute = ({ children }) => {
+  const [isAuth, setIsAuth] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://revoori-sathwika-reddy.onrender.com/api/auth/check-session', {
+      withCredentials: true
+    })
+    .then(res => setIsAuth(res.data.loggedIn))
+    .catch(() => setIsAuth(false));
+  }, []);
+
+  if (isAuth === null) return <p>Loading...</p>;
+
+  return isAuth ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
