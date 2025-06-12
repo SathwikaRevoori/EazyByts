a25057d8bb18891df3d5e5091fe4b491b8ac6c56
SathwikaRevoori <23071a12b7@vnrvjiet.in>
2025-06-12 14:45:39 +0530

    Fix: updated CORS to allow Netlify

--- portfolio-cms/server/server.js
+++ portfolio-cms/server/server.js
@@ -1,12 +1,28 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+
+const mongoose = require('mongoose');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const app = express();
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://cheery-seahorse-dd756b.netlify.app'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
+  credentials: true
+}));
+
 const PORT = process.env.PORT || 5000;
 
 // ======================
